bf411314465b7feab35cd6287dda8f8347615b4b
ankushsdei <ankushs@smartdatainc.net>
2019-07-13 00:41:24 +0530

    vue Laravel

---
+++ resources/views/test.blade.php
@@ -0,0 +1,18 @@
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Testing</title>
+	</head>
+	<body>
+		<h1>Welcome</h1>
+		<center>Hi</center>
+		@if()
+		{
+			echo "Hello";
+		}
+@foreach()
+{
+	echo "Right";
+}
+	</body>
+	</html>
